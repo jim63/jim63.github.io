52c3c2749af076d808f102f06143902c68ad1c3c
jim63 <b99611015@g.ntu.edu.tw>
2019-03-04 20:47:45 +0800

    fp

--- Jim/stylish/facebookJavaScriptSDK.js
+++ Jim/stylish/facebookJavaScriptSDK.js
@@ -71,8 +71,7 @@ function fbLogout() {
     FB.logout(function (response) {
       // Person is now logged out
       document.cookie = "userdata=; expires=Thu, 01 Jan 1970 00:00:00 GMT";
+      window.location.href = `./index.html`;
     });
   })
-
-
 }
