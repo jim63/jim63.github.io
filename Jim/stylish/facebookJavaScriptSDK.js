2fabbe73a291cd469dd6765eab1e2f31f79abaf8
jim63 <b99611015@g.ntu.edu.tw>
2019-03-04 23:08:34 +0800

    fp

--- Jim/stylish/facebookJavaScriptSDK.js
+++ Jim/stylish/facebookJavaScriptSDK.js
@@ -30,26 +30,6 @@ function checkConnected() {
       FB.getLoginStatus(function (response) {
         console.log('getLoginStatus2', response.authResponse.accessToken);
 
-        function httpGetAsync(theUrl) {
-          var xmlHttp = new XMLHttpRequest();
-          xmlHttp.onreadystatechange = function () {
-            if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-              console.log(JSON.parse(xmlHttp.responseText));
-              let userdata = {};
-              userdata.name = JSON.parse(xmlHttp.responseText).name;
-              userdata.email = JSON.parse(xmlHttp.responseText).email;
-              userdata.picture = JSON.parse(xmlHttp.responseText).picture.data.url;
-              console.log(userdata);
-              document.cookie = `userdata = ${JSON.stringify(userdata)}`;
-            }
-          }
-          xmlHttp.open("GET", theUrl, true); // true for asynchronous
-          xmlHttp.send();
-        }
-
-        let URL =
-          `https://graph.facebook.com/v3.2/me?fields=id,name,picture.type(large),email&access_token=${response.authResponse.accessToken}`
-        httpGetAsync(URL)
       });
       window.location.href = `./member.html?isLogin=true`;
     } else {
@@ -62,6 +42,26 @@ function fbLogin() {
   FB.login(function (response) {
     // Handle the response object, like in statusChangeCallback() in our demo
     // code.
+    function httpGetAsync(theUrl) {
+      var xmlHttp = new XMLHttpRequest();
+      xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+          console.log(JSON.parse(xmlHttp.responseText));
+          let userdata = {};
+          userdata.name = JSON.parse(xmlHttp.responseText).name;
+          userdata.email = JSON.parse(xmlHttp.responseText).email;
+          userdata.picture = JSON.parse(xmlHttp.responseText).picture.data.url;
+          console.log(userdata);
+          document.cookie = `userdata = ${JSON.stringify(userdata)}`;
+        }
+      }
+      xmlHttp.open("GET", theUrl, true); // true for asynchronous
+      xmlHttp.send();
+    }
+
+    let URL =
+      `https://graph.facebook.com/v3.2/me?fields=id,name,picture.type(large),email&access_token=${response.authResponse.accessToken}`
+    httpGetAsync(URL)
     console.log('fbLogin', response.status);
   });
 }
