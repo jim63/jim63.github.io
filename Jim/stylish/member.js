6236e72c3cb716f7d2ccde01c14a332781eeb0ba
jim63 <b99611015@g.ntu.edu.tw>
2019-03-04 17:34:40 +0800

    fp

---
+++ Jim/stylish/member.js
@@ -0,0 +1,24 @@
+function readCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+
+
+let cookieUser = JSON.parse(readCookie('userdata'));
+let name = cookieUser.name;
+let email = cookieUser.email;
+let picture = cookieUser.picture;
+
+let member__Container__Img = document.querySelector('.member__Container__Img img');
+let member__Container__Name = document.querySelector('.member__Container__Name');
+let member__Container__Email = document.querySelector('.member__Container__Email');
+
+member__Container__Name.innerHTML = name;
+member__Container__Email.innerHTML = Email;
+member__Container__Img.src = picture;
