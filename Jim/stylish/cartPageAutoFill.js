6d1e9871313e4fc434481c54ad09be1aeb3a3b25
jim63 <b99611015@g.ntu.edu.tw>
2019-03-05 11:22:39 +0800

    tt

---
+++ Jim/stylish/cartPageAutoFill.js
@@ -0,0 +1,13 @@
+let cart__Container__Sub__Order__Input__Name = document.querySelector('.cart__Container__Sub__Order__Input__Name');
+let cart__Container__Sub__Order__Input__Email = document.querySelector('.cart__Container__Sub__Order__Input__Email');
+
+function getCookie(name) {
+  var value = "; " + document.cookie;
+  var parts = value.split("; " + name + "=");
+  if (parts.length == 2) return parts.pop().split(";").shift();
+}
+
+let userdata = JSON.parse(getCookie('userdata'));
+
+cart__Container__Sub__Order__Input__Name.innerHTML = userdata.name;
+cart__Container__Sub__Order__Input__Email.innerHTML = userdata.email;
